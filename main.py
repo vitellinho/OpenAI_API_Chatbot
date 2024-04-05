27ef6ae66e95e3628a5408a088fd8e12403fc8f2
vitellinho <vitelloluigi@hotmail.com>
2024-04-05 10:35:11 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,19 @@
+import openai
+
+# API-Key
+client = openai.OpenAI(api_key="") # "" <- eigenen API-Key einsetzen
+
+# Hinweis zum beenden des Chatbots
+print("Zum schließen des Chatbots, schreibe 'Beenden'\n")
+
+# Initalisierung des Chatbots + Print-Befehl für Output
+eingabe = None
+while eingabe != "Beenden":
+  eingabe = input("Eingabe: ")
+  response = client.chat.completions.create(
+    model="gpt-3.5-turbo",
+    messages=[
+      {"role": "user", "content": eingabe}
+    ]
+  )
+  print(f"Output: {response.choices[0].message.content}\n")
